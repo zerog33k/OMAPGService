7356614b8c26be75ab81f7e960f2973438bc651f
Rob Stevenson <rob@zerogeek.net>
2017-10-08 15:06:32 -0500

    Initial creation - device service does things

---
+++ OMAPGService/Controllers/DeviceController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OMAPGService.Models;
+
+namespace OMAPGService.Controllers
+{
+    [Route("api/[controller]")]
+    public class DeviceController : Controller
+    {
+        private readonly OMAPGContext _context;
+
+        public DeviceController(OMAPGContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Device> Get()
+        {
+            return _context.Devices;
+        }
+
+		[HttpGet("{id}", Name = "GetDevice")]
+		public IActionResult GetById(long id)
+		{
+            var item = _context.Devices.FirstOrDefault(t => t.Id == id);
+			if (item == null)
+			{
+				return NotFound();
+			}
+			return new ObjectResult(item);
+		}
+
+
+
+        [HttpPut]
+        public IActionResult Create([FromBody]Device value)
+        {
+            if (value == null)
+			{
+				return BadRequest();
+			}
+
+            _context.Devices.Add(value);
+			_context.SaveChanges();
+
+            return CreatedAtRoute("GetDevice", new { id = value.Id }, value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OMAPGService/Migrations/20171002030809_createMySql.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace OMAPGService.Migrations
+{
+    public partial class createMySql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeviceId = table.Column<string>(type: "longtext", nullable: true),
+                    OSType = table.Column<int>(type: "int", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
