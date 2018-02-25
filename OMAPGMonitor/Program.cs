0de30567bbdf88c698142ec50122267e4e10ffb1
Rob <rob@zerogeek.net>
2018-02-24 21:11:38 -0600

    better query for last timestamp?

--- OMAPGMonitor/Program.cs
+++ OMAPGMonitor/Program.cs
@@ -40,12 +40,8 @@ namespace OMAPGMonitor
             var minTimestamp = (long)(DateTime.UtcNow.AddMinutes(-5.0).Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
             var context = new OMAPGContext();
             context.ConnectString = config.DataAccessPostgresqlProvider;
-            var lastTimestamp = minTimestamp;
-            if (context.Pokemon.Count() > 0)
-            {
-                var maxTimestamp = context.Pokemon.Max(p => p.timestamp);
-                lastTimestamp = minTimestamp > maxTimestamp ? minTimestamp : maxTimestamp;
-            }
+            var maxTimestamp = context.Pokemon.FromSql("select * from public.\"Pokemon\" where timestamp = (select max(timestamp) from public.\"Pokemon\");").FirstOrDefault()?.timestamp ?? minTimestamp;
+            var lastTimestamp = minTimestamp > maxTimestamp ? minTimestamp : maxTimestamp;
             Console.WriteLine("Loading data...");
             await ServiceLayer.SharedInstance.LoadData(lastTimestamp);
             Console.WriteLine($"Loaded {ServiceLayer.SharedInstance.Pokemon.Count} Pokemon, {ServiceLayer.SharedInstance.Gyms.Count} Gyms, and {ServiceLayer.SharedInstance.Raids.Count} Raids!");
